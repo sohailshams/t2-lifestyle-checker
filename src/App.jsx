f810122ec408d66be5aa33ee53db706fb28e6177
sohailshams <sohailshamas@yahoo.com>
2023-09-08 18:45:19 +0100

    Installed axios and added getPatient function to fetch patient data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getPatient } from "./assets/api/api";
 
 function App() {
+  useEffect(() => {
+    getPatient(111222333)
+      .then((patient) => console.log(patient))
+      .catch((err) => console.log(err.response.data.msg));
+  }, []);
   return (
     <>
       <p className="underline text-red-500">T2 LifeStyle</p>
---
+++ src/assets/api/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const t2LifeStyleApi = axios.create({
+  baseURL: "https://t2-lifestyle.onrender.com/api",
+});
+
+export function getPatient(nhs_number) {
+  return t2LifeStyleApi
+    .get(`/patients/${nhs_number}`)
+    .then((res) => res.data.patient);
+}
+
+// .catch((err) => console.log(err.response.data.msg));
