c5a9030b398aba1b534b9621b0b110d818abd589
sohailshams <sohailshamas@yahoo.com>
2023-09-09 10:29:14 +0100

    Added Nav component.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <nav className="h-12 bg-[#333] text-white flex items-center justify-center sticky top-0 z-50 font-semibold">
+      T2 Lifestyle Checker
+    </nav>
+  );
+}
+
+export default Nav;
