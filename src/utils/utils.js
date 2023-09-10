8eec10997dcf2c302b174d0e40a4f567292777ac
sohailshams <sohailshamas@yahoo.com>
2023-09-10 17:42:33 +0100

    Updated return statement to return true or false with error message.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,7 +2,7 @@ export function messageToPatient(patientInputData, patientDbData) {
   const { nhs_number, surname, dob } = patientInputData;
   const dbPatientSize = Object.keys(patientDbData).length;
   if (dbPatientSize === 0) {
-    return "Your details could not be found";
+    return [false, "Your details could not be found"];
   }
   if (parseInt(nhs_number) === patientDbData.nhs_number) {
     if (
@@ -10,16 +10,18 @@ export function messageToPatient(patientInputData, patientDbData) {
       dob.toLowerCase() === patientDbData.dob.toLowerCase() &&
       patientDbData.age < 16
     ) {
-      return "You are not eligble for this service";
+      return [false, "You are not eligble for this service"];
     }
     if (
       surname.toLowerCase() === patientDbData.surname_name.toLowerCase() &&
       dob.toLowerCase() === patientDbData.dob.toLowerCase() &&
       patientDbData.age > 16
     ) {
-      return "You found - go to part two";
+      return [true, "You found - go to part two"];
     } else {
-      return "Your details could not be found";
+      return [false, "Your details could not be found"];
     }
+  } else {
+    return [false, "Your details could not be found"];
   }
 }
