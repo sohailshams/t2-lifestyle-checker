f2060f1df958cb8a59e913d7845db6e803cb4656
sohailshams <sohailshamas@yahoo.com>
2023-09-11 09:32:45 +0100

    Added Link to navigate to home page.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
   return (
     <nav className="h-12 bg-[#333] text-white flex items-center justify-center sticky top-0 z-50 font-semibold">
-      T2 Lifestyle Checker
+      <Link to="/">T2 Lifestyle Checker</Link>
     </nav>
   );
 }
