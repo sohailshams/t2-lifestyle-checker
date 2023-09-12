c8d1717e2ec1625229b662639b681fb2b61a82aa
sohailshams <sohailshamas@yahoo.com>
2023-09-12 11:58:30 +0100

    Removed unnecessary code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
-import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { getPatient } from "./api/api";
 import PatientInput from "./components/PatientInput";
 import Nav from "./components/Nav";
 import Questions from "./components/Questions";
 
 function App() {
-  // const [patientDbData, setPatientDbData] = useState({});
-  // useEffect(() => {
-  //   getPatient(111222333)
-  //     .then((patient) => setPatientDbData(patient))
-  //     .catch((err) => console.log(err.response.data.msg));
-  // }, []);
   return (
     <>
       <Nav />
